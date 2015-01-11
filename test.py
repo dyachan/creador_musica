692fd11f2bc82a3fc886683ab6a7d269e5565b42
dyachan <diego.yachan@gmail.com>
2015-01-11 00:45:24 -0300

    funciona crear secuencia, mostrarla y tocarla

--- test.py
+++ test.py
@@ -4,8 +4,8 @@
 from Secuencia import Secuencia
 from mingus.containers.Note import Note
 
-s = Secuencia(4, 4, 12, Note('C', 4), Secuencia.ESCALA[0])
-s.magiarSecuencia(1, 20, 70, 0, 0, 10)
+s = Secuencia(4, 4, 40, Note('C', 4), Secuencia.ESCALA[0])
+s.magiarSecuencia(1, 20, 70, 30, 20, 30)
 s.mostrar()
 print("")
 s.tocar()
